213932c2f913d8a59cbbbf06be64961336ff3517
Katherine <katherine.bensley.93@gmail.com>
2024-06-27 20:26:10 -0400

    model specs

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,23 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+console.log(process.env);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.PORT
+  }
+);
+
+    // try {
+    //   await sequelize.authenticate();
+    //   console.log('Connection has been established successfully.');
+    // } catch (error) {
+    //   console.error('Unable to connect to the database:', error);
+    // }
 
 module.exports = sequelize;
